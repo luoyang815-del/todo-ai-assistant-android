0f0a977b47a27d3536f69c9631dae39b8ee57540
luoyang815-del <luoyang815@gmail.com>
2025-10-17 17:07:53 +0800

    fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,6 +62,7 @@ dependencies {
     implementation(libs.ui.graphics)
     implementation(libs.ui.tooling.preview)
     implementation(libs.material3)
+    implementation(libs.material)
     implementation(libs.nav.compose)
 
     implementation(libs.room.runtime)
